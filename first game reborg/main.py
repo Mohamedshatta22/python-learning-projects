93228a049e303c8ff28c8a93537a187b5cf1938f
mohamed fathy shatta <podoshatta@gmail.com>
2026-06-24 15:10:18 +0300

    Create main.py
    Sunday, ‎June ‎1, ‎2025, ‏‎8:50:53 PM

---
+++ first game reborg/main.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+turn_left()
+for c in range(0,2):
+ move()
+turn_right()
+for c in range(0,2):
+ move()
+turn_right()
+for c in range(0,2):
+ move()
+turn_right()
+for c in range(0,2):
+ move()
+turn_left()
+
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=Alone&url=%2Fworlds%2Ftutorial_en%2Falone.json
