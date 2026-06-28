1df9bf89c24301a9c45c03bbd7fd00991852abc7
mohamed fathy shatta <podoshatta@gmail.com>
2026-06-28 13:52:00 +0300

    Create main.py

---
+++ 47 برنامج تشفير رسايل شيفرة قيصر Caesar cipher different versions in different time/main.py
@@ -0,0 +1,91 @@
+import string
+alphapit = string.ascii_lowercase
+punc = []
+punc_list=string.punctuation
+for m in punc_list:
+    punc.append(m)
+    punc.append(" ")
+    punc.append("?")
+word = input("please enter a word here ").lower()
+encrypted ="" 
+for x in word:
+   known_index =alphapit.index(x)
+   if known_index == 24:
+       known_index -=24
+       encrypted+=alphapit[known_index]
+   elif known_index == 25:
+       known_index -=24
+       encrypted+=alphapit[known_index]
+   
+   else:
+       known_index +=2
+       encrypted+=alphapit[known_index]
+       
+print(encrypted)
+-----------------------------------
+import string
+punc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " "]
+punc_scheck=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+punci=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " "]
+word = input("please enter a word here ").lower()
+encrypted ="" 
+for x in word:
+   if x in punc_scheck:
+    known_index =punc.index(x)
+    known_index+=2
+    encrypted+=punc[known_index]
+   elif x in punci:
+     encrypted+=x
+print(encrypted)
+-----------------------------------------
+import string
+punc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+punc_upper= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
+punci=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " ", '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " "]
+word = input("please enter a word here ")
+encrypted =""  
+for x in word: 
+  if x in punc:
+    known_index =punc.index(x)
+    known_index+=2
+    encrypted+=punc[known_index]
+      
+ 
+  elif x in punc_upper:
+       known_index =punc_upper.index(x)
+       known_index+=2
+       encrypted+=punc_upper[known_index]
+  elif x in punci:
+      encrypted+=x
+
+print(encrypted)
+======================================================
+import string
+word2 = input("please enter a word here ")
+shift_number2 = int(input("enter the number please "))
+def info (word,shift_number):
+ punc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+ punc_upper= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
+ punci=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " ", '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " "]
+ encrypted =""  
+ for x in word: 
+  if x in punc:
+    known_index =punc.index(x)
+    known_index+= shift_number
+    encrypted+=punc[known_index]
+      
+ 
+  elif x in punc_upper:
+       known_index =punc_upper.index(x)
+       known_index+=shift_number
+       encrypted+=punc_upper[known_index]
+  elif x in punci:
+      encrypted+=x
+ return encrypted
+ 
+print(info(word=word2,shift_number=shift_number2)
+)
