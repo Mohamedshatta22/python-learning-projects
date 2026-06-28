348231896a65a627e9a438c70d01fd6e47c2c88d
mohamed fathy shatta <podoshatta@gmail.com>
2026-06-28 12:25:02 +0300

    Create nain.py
    Wednesday, ‎August ‎13, ‎2025, ‏‎12:45:01 AM

---
+++ 25 small app on dictionary/nain.py
@@ -0,0 +1,14 @@
+student = {
+    1:"mohamed", 
+    2:"ahmed", 
+    3:"nour", 
+    4:"moustafa",
+    5:"maha"
+
+    
+
+}
+for x in range(1,6):
+    print(f"student id: {x}")
+    print(f"student name : {student[x]}")
+    print("___________________________")
