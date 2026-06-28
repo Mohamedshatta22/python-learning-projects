1cf8eeffbac6e926f053baa55be540072500bdd4
mohamed fathy shatta <podoshatta@gmail.com>
2026-06-28 12:20:26 +0300

    Create main.py
    ‎Saturday, ‎August ‎9, ‎2025, ‏‎7:45:39 PM

---
+++ 21 برنامج فك التشفير/main.py
@@ -0,0 +1,26 @@
+import string
+word2 = input("please enter a word here ")
+shift_number2 = int(input("enter the number please "))
+def info (word,shift_number):
+ punc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+ punc_upper= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
+ punci=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " ", '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', " "]
+ encrypted =""  
+ for x in word: 
+  if x in punc:
+    known_index =punc.index(x)
+    known_index+= (-shift_number)
+    encrypted+=punc[known_index]
+      
+ 
+  elif x in punc_upper:
+       known_index =punc_upper.index(x)
+       known_index+=(-shift_number)
+       encrypted+=punc_upper[known_index]
+  elif x in punci:
+      encrypted+=x
+ return encrypted
+ 
+print(info(word=word2,shift_number=shift_number2)
+)
